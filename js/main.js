f548482644d769cd53fb08feb4ebdebb19f159c2
Rasul <shiroeqky@gmail.com>
2021-11-13 15:37:30 +0500

    added some section

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+  freeMode: true,
+  loop: true,
+  slidesPerView: 9,
+  grabCursor: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
